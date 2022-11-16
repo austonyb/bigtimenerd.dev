f49e04925e1ba803a57ec5f6b3aa9bf52024409f
austonyb <auston.youngblood@gmail.com>
2022-11-16 11:07:20 -0700

    added an admin in auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
     }
 })
 
-const adminList = ['Auston', 'Janalie']
+const adminList = ['Auston', 'Janalie', 'Cameron']
 
 
 module.exports = {
